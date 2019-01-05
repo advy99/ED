4286947e048a8ca6e2d5ec787c6a8fc0c49171e6
Antonio David Villegas Yeguas <advy99@gmail.com>
2019-01-05 23:40:37 +0100

    eso no deberia haberse subido

--- practica_final/material/doc/html/annotated_dup.js
+++
@@ -1,7 +0,0 @@
-var annotated_dup =
-[
-    [ "bolsa_letras", "classbolsa__letras.html", "classbolsa__letras" ],
-    [ "conjunto_letras", "classconjunto__letras.html", "classconjunto__letras" ],
-    [ "letra", "classletra.html", "classletra" ],
-    [ "lista_palabras", "classlista__palabras.html", "classlista__palabras" ]
-];
